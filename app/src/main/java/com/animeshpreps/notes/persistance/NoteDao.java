abe5b2aafd598f74f3c42cbe1b5ec0af5f9c4a71
animesh <royanimesh2211@gmail.com>
2023-11-24 17:54:24 +0530

    Implemented DAO

---
+++ app/src/main/java/com/animeshpreps/notes/persistance/NoteDao.java
@@ -0,0 +1,32 @@
+package com.animeshpreps.notes.persistance;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.animeshpreps.notes.models.Note;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    long[] insertNotes(Note... notes);
+
+    @Query("SELECT * FROM notes")
+    LiveData<List<Note>> getNotes();
+
+    @Query("SELECT * FROM notes WHERE title LIKE :title")
+    List<Note> getNoteWithCustomQuery(String title);
+
+    @Delete
+    int delete(Note... notes);
+
+    @Update
+    int update(Note... notes);
+
+}
