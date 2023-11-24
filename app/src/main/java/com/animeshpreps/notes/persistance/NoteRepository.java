8a3ea153a253e631c87a31471daee7ecbdc9e745
animesh <royanimesh2211@gmail.com>
2023-11-24 18:16:31 +0530

    Created NoteRepository as a bridge

---
+++ app/src/main/java/com/animeshpreps/notes/persistance/NoteRepository.java
@@ -0,0 +1,31 @@
+package com.animeshpreps.notes.persistance;
+
+import android.content.Context;
+import androidx.lifecycle.LiveData;
+import com.animeshpreps.notes.models.Note;
+import java.util.List;
+
+public class NoteRepository {
+
+    private NoteDatabase mNoteDatabase;
+
+    public NoteRepository(Context context) {
+        mNoteDatabase = NoteDatabase.getInstance(context);
+    }
+
+    public void insertNoteTask(Note note) {
+
+    }
+
+    public void updateNote(Note note) {
+
+    }
+
+    public LiveData<List<Note>> retrieveNotesTask() {
+        return null;
+    }
+
+    public void deleteNote(Note note) {
+
+    }
+}
